9f596b78095c625f0a120906c993d3c904656b5d
Andreas Baschir <andreas.baschir@stud.etti.upb.ro>
2025-09-14 14:19:19 +0300

    src/main.py: Add gemini in POST route

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, HTTPException
+from google import genai
+from google.genai import types
+from middleware import RateLimiterMiddleware
 from pydantic import BaseModel, Field
 from typing import Literal, Optional
-from middleware import RateLimiterMiddleware
-app = FastAPI()
 
+client = genai.Client()
+
+app = FastAPI()
 app.add_middleware(RateLimiterMiddleware, max_requests=5, window_seconds=60)
 
 class RequestModel(BaseModel):
@@ -18,4 +22,20 @@ class ResponseModel(BaseModel):
 
 @app.post("/summarize", response_model=ResponseModel)
 async def summarize(request: RequestModel):
-    pass
+    if not request.text.strip():
+        raise HTTPException(status_code=400, detail="Text cannot be empty")
+    
+    user_prompt = f"Summarize the following text in a {request.length} {request.style} format:\n\n {request.text}"
+    if request.focus:
+        user_prompt += f" Emphasize the topic: {request.focus}."
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        config=types.GenerateContentConfig(
+            system_instruction="You are a helpful assistant that summarizes text based on user preferences."),
+        contents=f"{user_prompt}"
+    )
+
+    summary = response.text.strip()
+    meta = {"model": "gemini-2.5-flash", "length": request.length, "style": request.style, "focus": request.focus}
+    return ResponseModel(summary=summary, meta=meta)
